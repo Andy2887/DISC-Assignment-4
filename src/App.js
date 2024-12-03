f1daf9b6880560a1f521478f03b0b46dacf348fa
Andy2887 <yuanliheng05@gmail.com>
2024-12-03 13:05:52 -0600

    basic structure created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
+
+// Navigation bar definition
+function Nav(){
+  return(
+    <nav className = "nav">
+      <h1>DISC</h1>
+      <a href='home.html'>Home</a>
+      <a href='about.html'>About</a>
+      <a href='create.html'>Create</a>
+    </nav>
+  );
+}
+
+// title bar definition
+function Title(){
+  return(
+    <div class = "title">
+      <h1>Find Your Friends!</h1>
+    </div>
+  );
+}
+
+// profile card definition
+function ProfileCard(props){
+  return(
+    <div class = "profile">
+        <div class="text">
+          <p>{props.name}</p>
+          <hr />
+          <p>{props.description}</p>
+        </div>
+    </div>
+  );
+}
+
+
+
+// the main function
 function App() {
   return (
-    <h1>React App</h1>
+  <div>
+    <Nav />
+    <Title />
+    <ProfileCard name = "John Doe" description = "I am a student at UBC" />
+    <ProfileCard name = "Jane Cruise" description = "I am a student at SFU" />
+    <ProfileCard name = "Jack Smith" description = "I am a student at BCIT" />
+  </div>
   );
 }
 
